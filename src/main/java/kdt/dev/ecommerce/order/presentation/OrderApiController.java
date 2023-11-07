06a18e6dda329316f0ac06b0179ee5a233c73a10
EunChanNam <namhm23@gmail.com>
2023-11-08 02:08:09 +0900

    feat: 주문 API 명세

--- src/main/java/kdt/dev/ecommerce/order/presentation/OrderApiController.java
+++ src/main/java/kdt/dev/ecommerce/order/presentation/OrderApiController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kdt.dev.ecommerce.global.annotation.Login;
 import kdt.dev.ecommerce.global.resolver.dto.LoginInfo;
 import kdt.dev.ecommerce.order.application.usecase.OrderUseCase;
 import kdt.dev.ecommerce.order.presentation.dto.OrderRequest;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Order API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/orders")
@@ -20,9 +25,11 @@ public class OrderApiController {
 	private final OrderUseCase orderUseCase;
 	private final Mapper mapper;
 
+	@Operation(summary = "주문 API", description = "주문을 한다")
+	@ApiResponse(responseCode = "200", description = "주문 성공")
 	@PostMapping
 	public ResponseEntity<Boolean> order(
-		@Login LoginInfo loginInfo,
+		@Parameter(hidden = true) @Login LoginInfo loginInfo,
 		@RequestBody OrderRequest request
 	) {
 		orderUseCase.order(mapper.toOrderCommand(request, loginInfo.userId()));
--- src/main/java/kdt/dev/ecommerce/order/presentation/dto/OrderRequest.java
+++ src/main/java/kdt/dev/ecommerce/order/presentation/dto/OrderRequest.java
@@ -1,8 +1,14 @@
 package kdt.dev.ecommerce.order.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "주문 요청 스펙")
 public record OrderRequest(
+	@Schema(description = "상품 ID")
 	Long productId,
+	@Schema(description = "상세 아이템 ID")
 	Long itemDetailId,
+	@Schema(description = "주문 수량")
 	int quantity
 ) {
 }
