eae38c1ac2a93eb69c4639bf69ff9142ccd80817
EunChanNam <namhm23@gmail.com>
2023-11-07 18:28:45 +0900

    feat: Order 어그리게이션 Order, OrderProduct 구현

---
+++ src/main/java/kdt/dev/ecommerce/order/domain/entity/Order.java
@@ -0,0 +1,39 @@
+package kdt.dev.ecommerce.order.domain.entity;
+
+import static jakarta.persistence.ConstraintMode.*;
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kdt.dev.ecommerce.global.entity.BaseEntity;
+import kdt.dev.ecommerce.user.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "order")
+public class Order extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id", foreignKey = @ForeignKey(NO_CONSTRAINT))
+	private User user;
+
+	private int orderPrice;
+
+	public Order(User user, int orderPrice) {
+		this.user = user;
+		this.orderPrice = orderPrice;
+	}
+}
